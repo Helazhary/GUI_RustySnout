77f0d9f61a28566e2f2a3eca35916250103416f1
Tawfik96 <eslamtawfik@aucegypt.edu>
2024-05-07 02:03:30 +0300

    Integrating sqllite database logic + updating UI

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,89 @@
+
+use gtk::gio::ffi::GOutputVector;
+//use rusqlite::{params, Connection as sqlConnection /* , Result*/};
+use rusqlite::{Connection, Result};
+
 use gtk::ffi::gtk_grid_remove_row;
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Button, Label, Box, Orientation, Entry, CssProvider, StyleContext, Align, Grid};
 use gtk::gdk;
 
-fn main() {
+use std::sync::{Arc, Mutex};
+    
+fn main() -> Result<()> {
     let app = Application::new(Some("com.example.RustySnout"), Default::default());
     
-    app.connect_activate(|app| {
-        build_ui(app);
+    //----------------------------Start of DB----------------------------
+    let conn = Connection::open("data.db")?;
+
+    
+    let mut stmt = conn.prepare("SELECT process_name, time, block_number FROM App")?;
+
+    // Execute the SELECT query and collect the results into a vector of vectors
+    let mut data: Vec<Vec<String>> = Vec::new();
+    let rows = stmt.query_map([], |row| {
+        Ok(vec![
+            row.get::<_, String>(0)?, // process_name
+            row.get::<_, i64>(1)?.to_string(), // time (convert to string)
+            row.get::<_, i64>(2)?.to_string(), // block_number
+        ])
+    })?;
+
+    // Process the results and store them in the vector of vectors
+    for row in rows {
+        data.push(row?);
+    }
+
+    // Now data contains all the rows from the App table
+   // println!("{:?}", data);
+
+//------------------------------END od DB-----------------------------    
+
+    // let data = vec![
+    //     vec![
+    //         "connection".to_string(),
+    //         "ID".to_string(),
+    //         "Name".to_string(),
+    //         "Size".to_string(),
+    //         "Extra".to_string(),
+    //     ],
+    //     vec![
+    //         "c1".to_string(),
+    //         "0".to_string(),
+    //         "goog".to_string(),
+    //         "20".to_string(),
+    //         "lo".to_string(),
+    //     ],
+    //     vec![
+    //         "c2".to_string(),
+    //         "1".to_string(),
+    //         "firefox".to_string(),
+    //         "30".to_string(),
+    //         "lo".to_string(),
+    //     ],
+    //     vec![
+    //         "c3".to_string(),
+    //         "2".to_string(),
+    //         "amazon".to_string(),
+    //         "50".to_string(),
+    //         "lo".to_string(),
+    //     ],
+    // ];
+
+    app.connect_activate( move |app| {
+       let shared_content = Arc::new(Mutex::new(data.clone()));
+        
+        build_ui(app, shared_content.clone());
+        
     });
 
-    app.run();
-}
+     app.run();
 
-fn build_ui(app: &Application) {
+Ok(())
+
+}
+//process_data:Vec<&[&str]>
+fn build_ui(app: &Application, shared_content: Arc<Mutex<Vec<Vec<String>>>>) {
     let css=b"/* CSS */
     * { font-family: monospace; background-color: #1e1e1e; color: #ffffff; } \
                 #HeaderLabel { color: #00ff00; } \
@@ -42,12 +112,16 @@ fn build_ui(app: &Application) {
     let refresh_rate_entry = Entry::builder()
         .placeholder_text("Enter refresh rate in seconds (Min: 0.5)")
         .build();
+
     let next_button = Button::builder()
         .label("Next")
         .build();
+
+        
     let setup_content = Box::new(Orientation::Vertical, 5);
     setup_content.append(&refresh_rate_entry);
     setup_content.append(&next_button);
+    
 
     setup_window.set_child(Some(&setup_content));
     setup_window.show();
@@ -83,6 +157,10 @@ fn build_ui(app: &Application) {
     buttons_box.append(&button1);
     buttons_box.append(&button2);
     buttons_box.append(&button3);
+    
+    let label = Label::builder()
+    .build();
+    content.append(&label);
 
     content.append(&header_label);
     content.append(&display_label);
@@ -98,6 +176,7 @@ fn build_ui(app: &Application) {
             refresh_rate = 0.5;
         }
         header_label.set_text(&format!(" Refresh Rate: {}s", refresh_rate));
+        // header_label.set_text(&format!(" Refresh Rate: {}s", process_data));
         setup_window.hide();
         main_window.show();
     });
@@ -106,90 +185,88 @@ fn build_ui(app: &Application) {
     button1.connect_clicked({
         let display_label = display_label.clone();
         let data_grid = data_grid.clone();
+        let label=label.clone();
         move |_| {
             display_label.set_label("Displaying usage data by: Process:");
-                populate_data_grid(&data_grid, vec![
-                    &["Process", "ID", "Name", "Size"],
-                    &["p1", "0", "goog", "20"],
-                    &["p2", "1", "firefox", "30"],
-                    &["P3", "2", "amazon", "50"],
-            ]);
-        }
+            {
+                let content = shared_content.lock().unwrap().clone();
+                        
+           // Iterate over the data and populate the grid
+            for (row_index, row) in content.iter().enumerate() {
+                for (col_index, value) in row.iter().enumerate() {
+                    let label = Label::new(Some(value));
+                    data_grid.attach(&label, col_index as i32, row_index as i32, 1, 1);
+                }
+            }
+
+        
+              
+                
+            }
+    }
     });
 
     // Connect button2
-    button2.connect_clicked({
-        let display_label = display_label.clone();
-        let data_grid = data_grid.clone();
-        move |_| {
-            display_label.set_label("Displaying usage data by: Connection:");
-            populate_data_grid(&data_grid, vec![
-                &["connection", "ID", "Name", "Size", "Extra"],
-                &["c1", "0", "goog", "20", "lo"],
-                &["c2", "1", "firefox", "30", "lo"],
-                &["c3", "2", "amazon", "50", "lo"],
-            ]);
-        }
-    });
+    // button2.connect_clicked({
+    //     let display_label = display_label.clone();
+    //     let data_grid = data_grid.clone();
+    //     move |_| {
+    //         display_label.set_label("Displaying usage data by: Connection:");
+    //         populate_data_grid(&data_grid, vec![
+    //             &["connection", "ID", "Name", "Size", "Extra"],
+    //             &["c1", "0", "goog", "20", "lo"],
+    //             &["c2", "1", "firefox", "30", "lo"],
+    //             &["c3", "2", "amazon", "50", "lo"],
+    //         ]);
+    //     }
+    // });
 
-    // Connect button3
-    button3.connect_clicked({
-        let display_label = display_label.clone();
-        let data_grid = data_grid.clone();
-        move |_| {
-            display_label.set_label("Displaying usage data by: Remote-Address:");
-            populate_data_grid(&data_grid, vec![
-                &["remote", "ID", "Name"],
-                &["r1", "0", "goog"],
-                &["r2", "1", "firefox"],
-                &["r3", "2", "amazon"],
-            ]);
+//     // Connect button3
+//     button3.connect_clicked({
+//         let display_label = display_label.clone();
+//         let data_grid = data_grid.clone();
+//         move |_| {
+//             display_label.set_label("Displaying usage data by: Remote-Address:");
+//             populate_data_grid(&data_grid, vec![
+//                 &["remote", "ID", "Name"],
+//                 &["r1", "0", "goog"],
+//                 &["r2", "1", "firefox"],
+//                 &["r3", "2", "amazon"],
+//             ]);
 
-        }
-    });
-}
+//         }
+//     });
+// }
 
-// Function to populate the data grid with dummy data
-// fn populate_data_grid(data_grid: &Grid, data: Vec<(&str, &str)>) {
 
-//     for j in 0..10{
-//         data_grid.remove_row(j);
-//     }
 
-//     for (row, (label_text, value_text)) in data.iter().enumerate() {
-//         let label_label = Label::new(Some(*label_text));
-//         let value_label = Label::new(Some(*value_text));
-//         data_grid.attach(&label_label, 0, row as i32, 1, 1);
-//         data_grid.attach(&value_label, 1, row as i32, 1, 1);
+
+
+
+
+
+
+// fn populate_data_grid(label: &Label, shared_content: Arc<Mutex<String>>) {
+//     // Clear previous contents of the data grid
+//     //     for j in 0..20{
+//     //     grid.remove_row(j);
+//     //     grid.remove_column(j)
+//     // }
+
+//     // Iterate over the data and populate the grid
+//     // for (row_index, row) in data.iter().enumerate() {
+//     //     for (col_index, &value) in row.iter().enumerate() {
+//     //         let label = Label::new(Some(value));
+//     //         grid.attach(&label, col_index as i32, row_index as i32, 1, 1);
+//     //     }
+//     // }
+
+//     // Set initial text of the label to the shared content
+//     {
+//         let content = shared_content.lock().unwrap().clone();
+//         label.set_text(&content)
 //     }
 // }
 
-fn populate_data_grid(grid: &Grid, data: Vec<&[&str]>) {
-    // Clear previous contents of the data grid
-        for j in 0..20{
-        grid.remove_row(j);
-        grid.remove_column(j)
-    }
-
-    // Iterate over the data and populate the grid
-    for (row_index, row) in data.iter().enumerate() {
-        for (col_index, &value) in row.iter().enumerate() {
-            let label = Label::new(Some(value));
-            grid.attach(&label, col_index as i32, row_index as i32, 1, 1);
-        }
-    }
-
-    // Iterate over the data and populate the grid (with adding classes for each row to facilitate styling)
-    // for (row_index, row_data) in data.iter().enumerate() {
-    //     let row = Box::new(Orientation::Horizontal, 0); // Create a new row box
-    //     row.set_css_classes( &["grid-row"]); // Add the grid-row class to the row
-    
-    //     for (col_index, &value) in row_data.iter().enumerate() {
-    //         let label = Label::new(Some(value));
-    //         row.append(&label); // Append label to the row
-    //     }
-    
-    //     grid.attach(&row, 0, row_index as i32, 1, 1); // Attach the row to the grid
-    // }
 }
 
